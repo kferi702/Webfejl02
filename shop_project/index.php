f49426f9acb6189076cd675014e16a3a5fe61dae
Juhász Bence Zsolt <sz7juhben@vasvari.hu>
2018-12-12 16:15:52 +0100

    + Stíluslap létrehozás és csatolás

--- shop_project/index.php
+++ shop_project/index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Bejelentkezés | Webshop</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
     </head>
     <body>
         <div id="content">
