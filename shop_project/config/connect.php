758a78689af3d3336f2b191d8e2c1be929f66db1
Juhász Bence Zsolt <sz7juhben@vasvari.hu>
2018-12-12 17:40:21 +0100

    + Hibaüzenet módosítása

--- shop_project/config/connect.php
+++ shop_project/config/connect.php
@@ -8,7 +8,7 @@
     $connection = new mysqli($server, $username, $password, $database, $port);
     
     if ($connection -> connect_errno) {
-        die("Nem sikerült csatlakozni az adatbázishoz!");
+        die("Nem sikerült csatlakozni az adatbázishoz!".$connection->connect_error);
     }
     
     $connection->set_charset("utf8");
