9d70facd3b27c13e053791c1679883504d2a4d81
Juhász Bence Zsolt <sz7juhben@vasvari.hu>
2019-01-05 13:35:44 +0100

    Adatbázis interakció

--- shop_project/regController.php
+++ shop_project/regController.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    require_once('config/connect.php');
     $_SESSION['regErr'] = [];
     require_once('config/functions.php');
     
@@ -9,5 +10,16 @@
     }
     
     $username = readPost('username');
+    
+    $sql = 'SELECT username FROM user WHERE username = "$username"';
+    $res = $connection->query($sql);
+    
+    if ($res->num_rows > 0) {
+        
+        $_SESSION['regErr']['username'] = 'Már létezik ilyen felhasználónév!';
+        header('Location: reg.php');
+    }
+    
+    $sql = "INSERT INTO user (username, password, reg_date, active) VALUES ('$username', '12345', 'CURDATE()')";
     header('Location: reg.php');
     
