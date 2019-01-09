8f732ec44d6eaafabec31d55cc4630f84efd2fa0
Bence Juhász <juhasz.bence@outlook.hu>
2019-01-09 15:24:08 +0100

    Connect.php javítás

--- shop_project/config/connect.php
+++ shop_project/config/connect.php
@@ -1,14 +1,11 @@
 <?php
 $server = "localhost";
-$user = "root";
-$password = "";
-$dbName = "shop";
+$user = "esti_szoftverf";
+$password = "esti_szoftverf";
+$dbName = "esti_shop";
 $port = "3306";
 $conn = new mysqli($server,$user,$password,$dbName,$port);
 if ($conn -> connect_errno){
     die("Nem sikerült csatlakozni!".$conn -> connect_error);
 }
 $conn ->set_charset("utf8");
-
-
-
