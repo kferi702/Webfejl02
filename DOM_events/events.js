a43804ff6f0eba799238270772a41434a171d8fd
Bence Juhász <juhasz.bence@outlook.hu>
2019-01-12 11:38:11 +0100

    jQuery DOM manipuláció

--- DOM_events/events.js
+++ DOM_events/events.js
@@ -4,5 +4,13 @@ function bovit(){
 	console.log(urlap['szoveg'].value);
 	
 	let p = document.getElementById("bekezdesjs");
-	p.innerHTML += urlap['szoveg'].value;
+	p.innerHTML += '<br/>' + urlap['szoveg'].value;
 }
+
+
+$(document).ready(function(){	
+	$("[name = 'bovitjquery']").click(function(){
+		let val = $('#beformjquery input:first').val();
+		$('#bekezdesjquery').html(val);
+	});
+});
