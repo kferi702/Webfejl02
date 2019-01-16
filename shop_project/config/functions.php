e6faad6b06c2b259f06f4fc3816c40a22b3b7536
Bence Juhász <juhasz.bence@outlook.hu>
2019-01-16 16:13:34 +0100

    Update functions.php

--- shop_project/config/functions.php
+++ shop_project/config/functions.php
@@ -7,8 +7,11 @@ function dd($var) {
 
 function readPost($kulcs) {
     $temp = trim($_POST[$kulcs]);
+
     if (!empty($temp)) {
         return $temp;
+        echo "Nem üres";
+        die();
     } else {
         $_SESSION['regErr'][$kulcs] = 'Kitöltetlen mező!';
     }
