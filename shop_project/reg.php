4fb83caf3cad3cdcdaf0ca018c6f84bcd15c2bb7
Bence Juhász <juhasz.bence@outlook.hu>
2019-01-16 16:07:58 +0100

    Update reg.php

--- shop_project/reg.php
+++ shop_project/reg.php
@@ -1,9 +1,12 @@
 <?php
 
 session_start();
+session_unset();
 $_SESSION['regErr'] = [];
+
 require_once("config/functions.php");
 require_once('config/connect.php');
+//Regisztrál...
 if (!isset($_POST['regisztracio'])) {
     header("Location: index.php");
     die();
@@ -20,15 +23,16 @@ $username = readPost('username');
  */
 $sql = "SELECT username FROM user WHERE username = '$username'";
 $res = $conn->query($sql);
-if ($res->num_rows > 0) {
+if (($res->num_rows > 0) || ($username == "")) {
     $_SESSION['regErr']['username'] = 'Már létezik ilyen felhasználó!';
     header("Location: regisztracio.php");
+    die();
 }
 
-$pwd = readPost('password');
+$pwd = readPost('pwd');
 $pwd = password_hash($pwd, PASSWORD_BCRYPT);
 $_SESSION['regErr']['username'] = "";
-$sql = "INSERT INTO user (username, password, reg_date, active) VALUES ('$username', '$pwd', CURDATE(), 1)";
+$sql = "INSERT INTO user (username,password,reg_date,active) VALUES ('$username','$pwd',CURDATE(),1)";
 $res = $conn->query($sql);
 
 $_SESSION['siker'] = true;
