803fc4709d4191e6055c4ea64ef9336c20e4fb5d
Bence Juhász <juhasz.bence@outlook.hu>
2019-01-16 16:42:37 +0100

    jQuery letöltés és behivatkozás

--- shop_project/index.php
+++ shop_project/index.php
@@ -11,7 +11,9 @@ if (isset($_SESSION['uid'])) {
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link rel="stylesheet" type="text/css" href="css/shop.css" >
+        <link rel="stylesheet" type="text/css" href="css/shop.css"/>
+        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
     </head>
     <body>
         <div id="content">
