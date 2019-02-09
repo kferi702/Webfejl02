1728a8c6cc0f91117f1aa38aa58f1cd3a8a4f951
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 09:23:36 +0100

    Update connect.php

--- AJAX/config/connect.php
+++ AJAX/config/connect.php
@@ -1,9 +1,11 @@
 <?php
 
 $connection = new mysqli('localhost', 'root', '', 'ajax', '3306');
+
 if ($connection->connect_errno) {
     die('Nem sikerült csatlakozni!');
 }
+
 if (!$connection->set_charset("utf8")) {
     echo 'Nem sikerült beállítani a karakterkódolást!';
 }
