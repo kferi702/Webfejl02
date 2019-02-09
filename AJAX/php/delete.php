1843f49b1465cc821cceab21fff043a2fd8cf6fb
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 10:45:50 +0100

    Update delete.php

--- AJAX/php/delete.php
+++ AJAX/php/delete.php
@@ -4,5 +4,13 @@ require_once('../config/connect.php');
 
 if (isset($_POST['del'])) {
     $id = $_POST['del'];
-    $sql = "DELETE FROM user WHERE ID = '$id'";
+    $sql = "DELETE FROM user WHERE ID = '$id';";
+
+    $result = $connection->query($sql);
+
+    if (!$result) {
+        die("Hiba a törlés során!");
+    }
 }
+
+$connection-close();
