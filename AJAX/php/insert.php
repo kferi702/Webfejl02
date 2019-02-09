874e63a94787932afed70345d46800519bf2d69e
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 11:38:15 +0100

    Update insert.php

--- AJAX/php/insert.php
+++ AJAX/php/insert.php
@@ -2,9 +2,13 @@
 
 require_once('../config/connect.php');
 
-$sql = "INSERT INTO WHERE";
-$result = $connection->query($sql);
+if (isset($_POST['knev'])) {
+    $vnev = $_POST['veznev'];
+    $knev = $_POST['knev'];
+}
 
+$sql = "INSERT INTO user(vezeteknev, keresztnev) VALUES (vezeteknev, keresztnev);";
+$result = $connection->query($sql);
 
 if (!$result) {
     die("Hiba a felvitel során!");
