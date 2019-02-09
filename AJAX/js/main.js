69bbf272ef04467822097be4aa046b425c868a1d
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 11:03:02 +0100

    Update main.js

--- AJAX/js/main.js
+++ AJAX/js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
     olvas();
     $(document).on("click", ".torol", function () {
-        let id = $(this).attr('ID');
-        console.log(id);
+        let id = $(this).attr("ID");
+        $.post("php/del.php",
+                {
+                    del: id
+                },
+                function (valasz) {
+                    olvas();
+                });
     });
 });
 
