6173c13c9b0c52b553e38da3748132e8db30e847
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 11:34:25 +0100

    Update insert.php

--- AJAX/php/insert.php
+++ AJAX/php/insert.php
@@ -2,6 +2,12 @@
 
 require_once('../config/connect.php');
 
+$sql = "INSERT INTO WHERE";
+$result = $connection->query($sql);
 
+
+if (!$result) {
+    die("Hiba a felvitel során!");
+}
 
 $connection->close();
