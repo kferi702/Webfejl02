f0679a11009999d394e81d938362e1465d4bef6c
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 09:16:56 +0100

    init

---
+++ AJAX/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <title></title>
+    </head>
+    <body>
+        <h1>AJAX</h1>
+        <div id="container">
+
+        </div>
+    </body>
+</html>
