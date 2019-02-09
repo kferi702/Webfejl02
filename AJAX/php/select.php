b5cf5fc18ee8a07131b33ac5ddafac9c61a966f1
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 09:33:44 +0100

    Update select.php

--- AJAX/php/select.php
+++ AJAX/php/select.php
@@ -9,4 +9,12 @@ if (!$result) {
     die('Hiba a lekérdezés során!');
 }
 
+$html = '<table>'
+        . '<tr>Sorszám<th></th>'
+        . '<th>Vezetéknév</th>'
+        . '<th>Keresztnév</th>'
+        . '<th>Művelet</th></tr>';
+
+$html .= '</table>';
+
 $connection->close();
