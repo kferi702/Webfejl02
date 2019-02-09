73ef3cf53c9a06a8d210a2fc135f434bfc78691c
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 14:00:13 +0100

    refaktor

--- AJAX_2/php/delete.php
+++ AJAX_2/php/delete.php
@@ -2,15 +2,3 @@
 
 require_once('../config/connect.php');
 
-if (isset($_POST['del'])) {
-    $id = $_POST['del'];
-    $sql = "DELETE FROM jatekos WHERE ID = '$id';";
-
-    $result = $connection->query($sql);
-
-    if (!$result) {
-        die("Hiba a törlés során!");
-    }
-}
-
-$connection->close();
--- AJAX_2/php/insert.php
+++ AJAX_2/php/insert.php
@@ -2,16 +2,3 @@
 
 require_once('../config/connect.php');
 
-if (isset($_POST['knev']) && !empty($_POST['knev'])) {
-    $vnev = $_POST['vnev'];
-    $knev = $_POST['knev'];
-}
-
-$sql = "INSERT INTO jatekos(mez, nev, magassag, post) VALUES ('$vnev', '$knev');";
-$result = $connection->query($sql);
-
-if (!$result) {
-    die("Hiba a felvitel során!");
-}
-
-$connection->close();
