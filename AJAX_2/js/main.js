c5e6a806a146a79e45cb9014be751754b663963a
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 13:56:35 +0100

    Update main.js

--- AJAX_2/js/main.js
+++ AJAX_2/js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
     getPlayers();
+
+    $(document).on("click", "#player", function () {
+
+        let nev = $('#player').text();
+
+        if ((nev.length > 0) && !empty(nev)) {
+            $.ajax({
+                url: "php/select.php",
+                method: "post",
+                dataType: "TEXT",
+                data: {
+                    "player": nev
+                },
+                success: function (valasz) {
+                    olvas();
+                },
+                error: function (xhr) {
+                    alert(xhr.status);
+                }
+            });
+        }
+    });
 });
 function getPlayers() {
     $.get("php/select.php", function (valasz, status) {
