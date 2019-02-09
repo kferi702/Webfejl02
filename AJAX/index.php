3eb50170f49410063af6b23d4c2fe7061ee89de0
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 09:46:38 +0100

    Favicon

--- AJAX/index.php
+++ AJAX/index.php
@@ -7,6 +7,7 @@
         <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/main.js" type="text/javascript"></script>
+        <link rel="icon" type="image/x-icon" href="img/ajax.png"/>
         <title>AJAX</title>
     </head>
     <body>
