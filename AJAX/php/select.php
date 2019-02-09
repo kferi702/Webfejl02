c61b757f32012fe857e26a56332f0d9fe324211d
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-09 09:30:41 +0100

    Update select.php

--- AJAX/php/select.php
+++ AJAX/php/select.php
@@ -3,5 +3,10 @@
 require_once('../config/connect.php');
 
 $sql = 'SELECT * FROM user';
+$result = $connection->query($sql);
+
+if (!$result) {
+    die('Hiba a lekérdezés során!');
+}
 
 $connection->close();
