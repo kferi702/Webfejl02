382a785c2dc5dca0b362215acc818ebfeda0d21f
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-13 18:46:21 +0100

    Update adatok.php

--- AJAX_2/php/adatok.php
+++ AJAX_2/php/adatok.php
@@ -4,28 +4,28 @@ require_once('../config/connect.php');
 
 if (isset($_POST['mez'])) {
     $mez = $_POST['mez'];
-}
 
-$sql = "SELECT * FROM jatekos WHERE mez = '$mez';";
-$result = $connection->query($sql);
+    $sql = "SELECT * FROM jatekos WHERE mez = '$mez';";
+    $result = $connection->query($sql);
 
-if (!$result) {
-    die('Hiba a lekérdezés során!');
-}
+    if (!$result) {
+        die('Hiba a lekérdezés során!');
+    }
 
-if ($result->num_rows == 1) {
-    $html = "<ul>";
+    if ($result->num_rows == 1) {
+        $html = "<ul>";
 
-    /*
-     * fetch_row() => Számozott tömb
-     * fetch_assoc() => Asszociatív tömb
-     */
-    $row = $result->fetch_row();
-    $html .= "<li> {$row[0]}</li>"
-            . "<li> {$row[2]}</li>"
-            . "<li> {$row[3]}</li>"
-            . "</ul>";
-    echo $html;
+        /*
+         * fetch_row() => Számozott tömb
+         * fetch_assoc() => Asszociatív tömb
+         */
+        $row = $result->fetch_row();
+        $html .= "<li> {$row[0]}</li>"
+                . "<li> {$row[2]}</li>"
+                . "<li> {$row[3]}</li>"
+                . "</ul>";
+        echo $html;
+    }
 }
 
 $connection->close();
