78adc0f07f4599404d7a6cc8fd7c8b6420019a9c
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-13 18:11:18 +0100

    Update jegyzo.php

--- AJAX_2/php/jegyzo.php
+++ AJAX_2/php/jegyzo.php
@@ -1,2 +1,23 @@
 <?php
 
+require_once('../config/connect.php');
+
+$sql = 'SELECT mez, magassag, post FROM jatekos';
+$result = $connection->query($sql);
+
+if (!$result) {
+    die('Hiba a lekérdezés során!');
+}
+
+$html = "<table id='adatok'>";
+
+while ($row = $result->fetch_assoc()) {
+    $html .= "<th>Mezszám</th>";
+    $html .= "<th>Magasság</th>";
+    $html .= "<th>Poszt</th>";
+}
+
+$html .= "</table>";
+echo $html;
+
+$connection->close();
