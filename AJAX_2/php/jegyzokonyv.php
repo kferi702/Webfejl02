1123c35117bc59cff82a2c7c64d2a3ed9efc0fe6
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-13 19:15:50 +0100

    Update jegyzokonyv.php

--- AJAX_2/php/jegyzokonyv.php
+++ AJAX_2/php/jegyzokonyv.php
@@ -2,3 +2,26 @@
 
 require_once('../config/connect.php');
 
+if (isset($_POST['mez'])) {
+    $mez = $_POST['mez'];
+
+    $sql = "SELECT * FROM jegyzokonyv WHERE mez = '$mez';";
+    $result = $connection->query($sql);
+
+    if (!$result) {
+        die('Hiba a lekérdezés során!');
+    }
+
+    if ($result->num_rows == 1) {
+        $html = "<ul>";
+
+        $row = $result->fetch_row();
+        $html .= "<li>Mezszám: {$row[0]}</li>"
+                . "<li>Magasság: {$row[2]}</li>"
+                . "<li>Poszt: {$row[3]}</li>"
+                . "</ul>";
+        echo $html;
+    }
+}
+
+$connection->close();
