87333dbdb9ee2a89b03e80900239ff2524c28b68
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-20 16:34:56 +0100

    Update index.php

--- gallery/index.php
+++ gallery/index.php
@@ -2,7 +2,10 @@
 <html lang="hu">
     <head>
         <meta charset="utf-8"/>
-        <title></title>
+        <title>Galéria</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
     </head>
     <body>
 
