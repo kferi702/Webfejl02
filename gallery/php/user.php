746efcb00d7ae22b81d09a489a609aead7ca1d12
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-20 17:39:01 +0100

    Update user.php

--- gallery/php/user.php
+++ gallery/php/user.php
@@ -4,11 +4,24 @@ require_once('../config/connect.php');
 session_start();
 
 if (isset($_POST['enter'])) {
+
     $email = $_POST['email'];
     $pwd = $_POST['pwd'];
+
     $sql = "SELECT * FROM user WHERE email = ? AND password = ?;";
     $stmt = $connection->prepare($sql);
     $stmt->bind_param("ss", $email, $pwd);
+    $stmt->execute();
+
+    if ($stmt->num_rows == 1) {
+        $stmt->bind_result($id, $firstname, $lastname);
+        $stmt->fetch();
+        $_SESSION['userid'] = $id;
+    } else {
+        header('Location: index.php');
+    }
+
+    $stmt->close();
 }
 ?>
 <!DOCTYPE html>
