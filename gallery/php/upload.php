03d86887efcefe342f178c7760f46a01fcaf9eb9
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-20 18:29:49 +0100

    Update upload.php

--- gallery/php/upload.php
+++ gallery/php/upload.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-        <title>Galéria</title>
+        <title>Galéria | Képek feltöltés</title>
         <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>
         <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
         <script src="../js/bootstrap.min.js" type="text/javascript"></script>
