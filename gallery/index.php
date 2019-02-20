9a6599aaffb1aaee9fccfe575243ad8bd01b08d7
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-20 16:50:06 +0100

    Head kiszervezése másik fájlba.

--- gallery/index.php
+++ gallery/index.php
@@ -1,12 +1,8 @@
 <!DOCTYPE html>
 <html lang="hu">
     <head>
-        <meta charset="utf-8"/>
         <title>Galéria</title>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <link rel="icon" href="./img/gallery.png" type="image/x-icon"/>
+        <?php require_once('./templates/head.html'); ?>
     </head>
     <body>
         <div class="container">
