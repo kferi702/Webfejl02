ec2b5ff2328136ac6b2d4a800669dbe8b993c748
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-27 16:23:31 +0100

    oldal átirányÍtások.

--- gallery/php/pictures.php
+++ gallery/php/pictures.php
@@ -1,10 +1,31 @@
+<?php
+require_once('../config/connect.php');
+
+session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8"/>
-        <title></title>
+        <title>Galéria</title>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>
+        <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+        <link rel="icon" href="../img/gallery.png" type="image/x-icon"/>
     </head>
     <body>
-        <?php echo 'Hi!'; ?>
+        <nav class="navbar navbar-expand">
+            <ul class="navbar">
+                <p class="nav-item">
+                    <a href="pictures.php" class="nav-link">Képek megtekintése</a>
+                    <a href="upload.php" class="nav-link">Kép feltöltése</a>
+                    <?php
+                    if (isset($_SESSION['userid'])) {
+                        echo '<a href="logout.php" class="nav-link">Kilépés</a>'
+                        . '</p>';
+                    }
+                    ?>
+            </ul>
+        </nav>
     </body>
 </html>
