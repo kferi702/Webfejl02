d079f7c5f2fdcf7daabc57e6ae5c8682761c436d
Bence Juhász <juhasz.bence@outlook.hu>
2019-03-02 10:37:19 +0100

    Javítás.

--- JobLister/config/init.php
+++ JobLister/config/init.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-require_once('config/connect.php');
 require_once('config/functions.php');
 
 print_HTML('html/header.html');
--- JobLister/index.php
+++ JobLister/index.php
@@ -4,5 +4,4 @@ require_once('./config/functions.php');
 print_HTML('./html/header.html');
 print_HTML('./html/menu.html');
 print_HTML('./html/content.html');
-print_HTML('./html/login.html');
 print_HTML('./html/footer.html');
