e266afd0c3060c3b59e8a5e7e767653761ec874f
Bence Juhász <juhasz.bence@outlook.hu>
2019-03-02 11:30:29 +0100

    form alapértelmezett viselkedés letiltása.

--- JobLister/config/init.php
+++ JobLister/config/init.php
@@ -3,4 +3,4 @@
 session_start();
 $appLocation = $_SERVER['DOCUMENT_ROOT'] . '/Webfejl02/JobLister/';
 require_once($appLocation . '/config/functions.php');
-require_once($appLocation . 'config/connect.php');
+require_once($appLocation . '/config/connect.php');
