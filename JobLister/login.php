8d7b5230dce056ea56aefb4a91714459805ebf3b
Bence Juhász <juhasz.bence@outlook.hu>
2019-03-02 10:27:05 +0100

    Layout.

---
+++ JobLister/login.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once('./config/functions.php');
+require_once('./config/init.php');
+
+print_HTML('./html/header.html');
+print_HTML('./html/menu.html');
+print_HTML('./html/login.html');
+print_HTML('./html/footer.html');
