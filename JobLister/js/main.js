5ebf39a87615e58a32f520fa11a9a0ac76d760aa
Bence Juhász <juhasz.bence@outlook.hu>
2019-03-02 10:41:49 +0100

    $.post

--- JobLister/js/main.js
+++ JobLister/js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
     $('#enter').click(function () {
-        
-        $.post();
-    });
 
+        $.post({
+            type: "POST",
+            url: "php/log_in.php",
+            data: {
+
+            },
+            success: function (valasz) {
+
+            },
+            error: function (status) {
+                alert(status);
+            }
+        });
+    });
 });
